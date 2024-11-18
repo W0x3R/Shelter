adba4a400bc910f69306a589b76898f49c4a2ef6
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-11-18 15:54:20 +0300

    fix: thebase_src constant was created (Mon, Nov 18, 2024, 3:54:13 PM)

--- src/js/components/slider/markup/createSourceElement.js
+++ src/js/components/slider/markup/createSourceElement.js
@@ -1,3 +1,5 @@
-export const createSourceElement = (imgName, type) => `<source
-		srcset="./assets/images/pets-list/${imgName}@1x.${type} 1x,./assets/images/pets-list/${imgName}@2x.${type} 2x, ./assets/images/pets-list/${imgName}@3x.${type} 3x"
-		type="image/${type}">`
+export const createSourceElement = (BASE_SRC, type) => `
+<source
+	srcset="${BASE_SRC}@1x.${type} 1x,${BASE_SRC}@2x.${type} 2x, ${BASE_SRC}@3x.${type} 3x"
+	type="image/${type}">
+`
