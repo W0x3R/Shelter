750b2fa38b72c4827bb4103be9d1e4ee2f65e088
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-11-18 19:07:41 +0300

    feat: the clickrightendbutton function has been moved. (Mon, Nov 18, 2024, 7:07:36 PM)

--- src/js/components/pagination.js
+++ src/js/components/pagination.js
@@ -1,10 +1,5 @@
-import { itemsPerPage } from "./pagination/itemsPerPage/setValueItemsPerPageOnPageLoad"
-import paginationValues from "./paginationValues"
 import { loadItems } from "./pagination/markup/loadItems"
-import { setButtonsDisabled } from "./pagination/buttonsActions/setButtonsDisabled"
-import { setButtonsEnabled } from "./pagination/buttonsActions/setButtonsEnabled"
-import { setCurrentPageValue } from "./pagination/itemsPerPage/currentPageValue"
-import { setCurrentPageText } from "./pagination/markup/setCurrentPageText"
+
 import {
 	clickLeftStartButton,
 	left,
@@ -13,18 +8,8 @@ import {
 	rightEnd
 } from "./pagination/buttonsActions/clickLeftStartButton"
 import { clickLeftButton } from "./pagination/buttonsActions/clickLeftButton"
-import { getMaxPageValue, setMaxPageValue } from "./pagination/maxPage"
 import { clickRightButton } from "./pagination/buttonsActions/clickRightButton"
-
-const clickRightEndButton = () => {
-	setMaxPageValue(Math.ceil(paginationValues.length / itemsPerPage))
-	setCurrentPageValue(getMaxPageValue())
-	setCurrentPageText()
-
-	setButtonsDisabled(right, rightEnd)
-	setButtonsEnabled(leftStart, left)
-	loadItems()
-}
+import { clickRightEndButton } from "./pagination/buttonsActions/clickRightEndButton"
 
 leftStart.addEventListener("click", clickLeftStartButton)
 left.addEventListener("click", clickLeftButton)
---
+++ src/js/components/pagination/buttonsActions/clickRightEndButton.js
@@ -0,0 +1,19 @@
+import paginationValues from "../../paginationValues"
+import { setCurrentPageValue } from "../itemsPerPage/currentPageValue"
+import { itemsPerPage } from "../itemsPerPage/setValueItemsPerPageOnPageLoad"
+import { loadItems } from "../markup/loadItems"
+import { setCurrentPageText } from "../markup/setCurrentPageText"
+import { getMaxPageValue, setMaxPageValue } from "../maxPage"
+import { left, leftStart, right, rightEnd } from "./clickLeftStartButton"
+import { setButtonsDisabled } from "./setButtonsDisabled"
+import { setButtonsEnabled } from "./setButtonsEnabled"
+
+export const clickRightEndButton = () => {
+	setMaxPageValue(Math.ceil(paginationValues.length / itemsPerPage))
+	setCurrentPageValue(getMaxPageValue())
+	setCurrentPageText()
+
+	setButtonsDisabled(right, rightEnd)
+	setButtonsEnabled(leftStart, left)
+	loadItems()
+}
