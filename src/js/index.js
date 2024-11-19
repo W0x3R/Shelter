7055537efd53fa52a8fd46681fbba40078c8e303
Stanislau Vyrski <114683300+W0x3R@users.noreply.github.com>
2024-11-19 19:52:04 +0300

    feat: a production build is assembled. revised readme.md (Tue, Nov 19, 2024, 7:54:43 PM)

--- src/js/index.js
+++
@@ -1,4 +0,0 @@
-import * as burger from './components/burger'
-import * as popup from './components/popup'
-import * as slider from './components/slider'
-import * as checkClickOut from './components/checkClickOut'
